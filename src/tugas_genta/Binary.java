4825129d3a93129408307973a1eb8371717c1758
Genta27 <91246536+Genta27@users.noreply.github.com>
2021-10-04 12:25:16 +0700

    Add files via upload

---
+++ src/tugas_genta/Binary.java
@@ -0,0 +1,29 @@
+
+package tugas_genta;
+import java.util.Scanner;
+public class Binary {
+    public  class Converter
+ {
+   public static String toBinary(int num)
+    {
+        String binary="";
+        while(num>0)
+        {
+            binary=(num%2)+binary;
+            num/=2;
+        }
+    return binary;
+    } 
+}
+//ur code goes here
+
+
+public class Program {
+    public static void main(String[ ] args) {
+        Scanner sc = new Scanner(System.in);
+        int x = sc.nextInt();
+        System.out.print(Converter.toBinary(x));
+    }
+}
+    
+}
---
+++ src/tugas_genta/Tugas_genta.java
@@ -0,0 +1,15 @@
+
+package tugas_genta;
+import java.util.Scanner;
+public class Tugas_genta {
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int days = scanner.nextInt();
+        
+        //your code goes here
+        int hours = days*24
+        int minutes = hours*60
+             System.out.println(second)
+    }
+}
